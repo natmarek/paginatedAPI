55f72a7dd31f7bbf59267e8f940bd3b9ce9d5908
Ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-05-25 00:42:20 +0100

    Created request.rest and modified users variable

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express()
 
 const users = [
     {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
-    {id:1, name: 'User 1'},
+    {id:2, name: 'User 2'},
+    {id:3, name: 'User 3'},
+    {id:4, name: 'User 4'},
+    {id:5, name: 'User 5'},
+    {id:6, name: 'User 6'},
+    {id:7, name: 'User 7'},
+    {id:8, name: 'User 8'},
+    {id:9, name: 'User 9'},
+    {id:10, name: 'User 10'},
+    {id:11, name: 'User 11'},
+    {id:12, name: 'User 12'},
 ]
 
 
