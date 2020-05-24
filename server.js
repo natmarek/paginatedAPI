3435b0d92005fd48df1b106654604eac264d3bab
Ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-05-25 00:57:26 +0100

    Chaned string to int in page variable

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const users = [
 
 
 app.get('/users', (req, res) => {
-    const page = req.query.page
-    const limit = req.query.limit
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
 
     const startIndex =(page -1) * limit
     const endIndex = page * limit
