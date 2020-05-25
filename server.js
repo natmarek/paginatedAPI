d84afbff3c75ea41da0015e8601160336d0e3d04
Ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-05-25 02:03:47 +0100

    Changes in paginatedResults function

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/users', paginatedResults(User),(req, res) => {
 })
 
 function paginatedResults(model) {
-    return(req, res, next)=>{
+    return async (req, res, next)=>{
     const page = parseInt(req.query.page)
     const limit = parseInt(req.query.limit)
 
@@ -52,10 +52,16 @@ function paginatedResults(model) {
         }
     }
 
-    results.results= model.slice(startIndex,endIndex)
-
+    try{
+      results.results= await model.find().limit(limit).skip(startIndex).exec()  
+      res.paginatedResults = results
+      next()
+    } catch (e) {
+        res.status(500).json({message: e.message})
+    }
+    
     res.paginatedResults = results
-    next()
+    
     }
 }
 
