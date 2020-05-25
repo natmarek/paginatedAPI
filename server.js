6e12cc5bdae084db84a40d89ec9f3ebe046b0e27
Ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-05-25 01:57:54 +0100

    Added async function and users to the db

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ const User = require('./users')
 mongoose.connect('mongodb://localhost/pagination', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.once('open', async () => {
-    if await (User.countDocuments().exec() > 0)
+    if ( await User.countDocuments().exec() > 0) return
+
+    Promise.all([
+        User.create({ name: 'User 1'}),
+        User.create({ name: 'User 2'}),
+        User.create({ name: 'User 3'}),
+        User.create({ name: 'User 4'}),
+        User.create({ name: 'User 5'}),
+        User.create({ name: 'User 6'}),
+        User.create({ name: 'User 7'}),
+        User.create({ name: 'User 8'}),
+        User.create({ name: 'User 9'}),
+        User.create({ name: 'User 10'}),
+        User.create({ name: 'User 11'}),
+    ]).then(() => console.log('Added users'))
 })
 
 app.get('/users', paginatedResults(User),(req, res) => {
--- users.js
+++ users.js
@@ -8,39 +8,3 @@ const userSchema = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('User', userSchema)
-
-
-// const users = [
-//     {id:1, name: 'User 1'},
-//     {id:2, name: 'User 2'},
-//     {id:3, name: 'User 3'},
-//     {id:4, name: 'User 4'},
-//     {id:5, name: 'User 5'},
-//     {id:6, name: 'User 6'},
-//     {id:7, name: 'User 7'},
-//     {id:8, name: 'User 8'},
-//     {id:9, name: 'User 9'},
-//     {id:10, name: 'User 10'},
-//     {id:11, name: 'User 11'},
-//     {id:12, name: 'User 12'},
-// ]
-
-// const posts = [
-//     {id:1, name: 'Post 1'},
-//     {id:2, name: 'Post 2'},
-//     {id:3, name: 'Post 3'},
-//     {id:4, name: 'Post 4'},
-//     {id:5, name: 'Post 5'},
-//     {id:6, name: 'Post 6'},
-//     {id:7, name: 'Post 7'},
-//     {id:8, name: 'Post 8'},
-//     {id:9, name: 'Post 9'},
-//     {id:10, name: 'Post 10'},
-//     {id:11, name: 'Post 11'},
-//     {id:12, name: 'Post 12'},
-// ]
-
-
-// app.get('/posts', paginatedResults(posts), (req, res) => {
-//     res.json(res.paginatedResults)
-// })
