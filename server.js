d6ed5a084839474d8d4549aac8ac9e57312e6db2
Ripleycmd <53310862+ripleycmd@users.noreply.github.com>
2020-05-25 01:12:53 +0100

    Added  PaginatedResults middleware function and new array with user posts.

--- server.js
+++ server.js
@@ -16,9 +16,40 @@ const users = [
     {id:12, name: 'User 12'},
 ]
 
+const post = [
+    {id:1, name: 'Post 1'},
+    {id:2, name: 'Post 2'},
+    {id:3, name: 'Post 3'},
+    {id:4, name: 'Post 4'},
+    {id:5, name: 'Post 5'},
+    {id:6, name: 'Post 6'},
+    {id:7, name: 'Post 7'},
+    {id:8, name: 'Post 8'},
+    {id:9, name: 'Post 9'},
+    {id:10, name: 'Post 10'},
+    {id:11, name: 'Post 11'},
+    {id:12, name: 'Post 12'},
+]
+
+
+app.get('/posts', paginatedResults(posts), (req, res) => {
+
+
+
+    results.results= users.slice(startIndex,endIndex)
+    res.json(results)
+})
+
+
 
 app.get('/users', (req, res) => {
-    const page = parseInt(req.query.page)
+    
+    res.json(results)
+})
+
+function paginatedResults(model) {
+    return(req, res, next)=>{
+const page = parseInt(req.query.page)
     const limit = parseInt(req.query.limit)
 
     const startIndex =(page -1) * limit
@@ -26,17 +57,23 @@ app.get('/users', (req, res) => {
 
     const results = {}
 
-    results.next = {
+    if (endIndex < users.length) {
+       results.next = {
         page: page+1,
         limit: limit
+    } 
     }
+    
+
+    if (startIndex > 0) {
     results.previous = {
         page: page-1,
         limit: limit
+        }
     }
 
     results.results= users.slice(startIndex,endIndex)
-    res.json(results)
-})
+    }
+}
 
 app.listen(3000)
